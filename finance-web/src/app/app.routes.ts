8d4bc2b151e29f2fd610a1f73bd740db0077a154
wwov <wwov1997@gmail.com>
2026-04-08 02:25:33 -0400

    chore: agregar layout public con el contenido inicial

--- finance-web/src/app/app.routes.ts
+++ finance-web/src/app/app.routes.ts
@@ -1,9 +1,36 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './core/layout/layouts/auth-layout/auth-layout';
+import { PublicLayout } from './core/layout/layouts/public-layout/public-layout';
 
 export const routes: Routes = [
+  // {
+  //   path: 'products',
+  //   loadChildren: () =>
+  //     import('./products/products.routes').then((m) => m.productsRoutes),
+  // },
   {
-    path: 'products',
-    loadChildren: () =>
-      import('./products/products.routes').then((m) => m.productsRoutes),
+    path: '',
+    component: PublicLayout,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./features/public/public.routes').then(
+            (m) => m.PUBLIC_ROUTES
+          ),
+      },
+    ],
+  },
+
+  {
+    path: 'login',
+    component: AuthLayout,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./features/auth/auth.routes').then((m) => m.AUTH_ROUTES),
+      },
+    ],
   },
 ];
